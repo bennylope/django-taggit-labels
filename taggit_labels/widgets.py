46b9f431e6c69851268558248af174efc21f7417
Ben Lopatin <ben.lopatin@wellfireinteractive.com>
2014-06-07 16:53:15 -0400

    Fixes file paths in widget media

--- taggit_labels/widgets.py
+++ taggit_labels/widgets.py
@@ -32,12 +32,13 @@ class LabelWidget(TagWidget):
             else:
                 selected_tags.append((tag.name, ''))
 
-        tag_li = "".join(["<li data-tag-name='{0}' class={1}>{0}</li>".format(tag[0], tag[1]) for tag in selected_tags])
+        tag_li = "".join(["<li data-tag-name='{0}' class={1}>{0}</li>".format(
+            tag[0], tag[1]) for tag in selected_tags])
         tag_ul = "<ul{0}>{1}</ul>".format(list_attrs, tag_li)
         return mark_safe(u"{0}{1}".format(tag_ul, return_val))
 
     class Media:
         css = {
-            'all': ('taggit_widgets.css',)
+            'all': ('css/taggit_labels.css',)
         }
-        js = ('taggit_widgets.js',)
+        js = ('js/taggit_labels.js',)
